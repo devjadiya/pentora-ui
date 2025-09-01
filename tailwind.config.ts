5fda6dbb141ee5e8a4345b0d1fd120af09f45cbd
Dev Jadiya <devjadiya15@gmail.com>
2025-09-01 13:33:56 +0000

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,9 +58,9 @@ const config = {
         },
       },
       borderRadius: {
-        lg: 'calc(var(--radius) - 2px)',
-        md: 'calc(var(--radius) - 4px)',
-        sm: 'calc(var(--radius) - 6px)',
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
